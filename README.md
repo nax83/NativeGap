11a1b11cb10dc3f018afb7bec9ec6d44abdc6091
Alessandro Nax <ale.nassisi@gmail.com>
2013-06-26 23:37:45 +0200

    clean description draft

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ NativeGap
 It shows how to use native footer and header with Phonegap and JS
 
 
-The idea behind the project is to preserve all the business logic in js (which can be reused in iOS/Android/BB)
-while using some native controls. This simplify things such as scrolling while preserves fixed header and footer
+The idea behind the project is to preserve all the business logic in js (which can be reused on mobile platforms like iOS/Android/BB/WP8)
+while using native controls. This simplifies things and allows better performaces.
+
+For instance, it's possible to hook a js handler to a button in an header or a footer (for the android version the binding/unbinding is done through the view id).
+The native button is only in charge of forwarding its native click event to the javascript logic.
--- gen/it/ale/nativegap/R.java
+++ gen/it/ale/nativegap/R.java
@@ -21,14 +21,12 @@ public final class R {
         public static final int splash=0x7f020007;
     }
     public static final class id {
-        public static final int cliente=0x7f070003;
-        public static final int codice=0x7f070004;
         public static final int exit=0x7f070001;
-        public static final int footer=0x7f070006;
-        public static final int footexit=0x7f070007;
+        public static final int footer=0x7f070004;
+        public static final int footexit=0x7f070005;
         public static final int header=0x7f070000;
         public static final int logout=0x7f070002;
-        public static final int mainview=0x7f070005;
+        public static final int mainview=0x7f070003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
